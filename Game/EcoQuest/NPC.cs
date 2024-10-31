9ce1a8b9bcaf67bd4ae92cd492eb3fdb746ccae6
victorptrc <108744480+victorptrc@users.noreply.github.com>
2024-10-31 16:24:29 +0100

    separated NPC and Dialogue class

--- Game/EcoQuest/NPC.cs
+++ Game/EcoQuest/NPC.cs
@@ -1,43 +1,20 @@
-//This will be changed to a dialogue class
-
 namespace EcoQuest;
 public class NPC
 {
-    private List<Option> Options;
-    public NPC(List<Option> options)
+    public List<Dialogue> Dialogues { get; private set; }
+    public string Name { get; private set; }
+
+    public string NpcDescription { get; private set; }
+
+    public NPC(string name, string npcDescription, List<Dialogue> dialogues)
     {
-        Options = options;
+        Name = name;
+        NpcDescription = npcDescription;
+        Dialogues = dialogues;
     }
 
-    public void Talk()
+    public void Talk(int dialogueNumber)
     {
-        System.Console.WriteLine(NpcReply.GREETING);
-
-        var isTalkingTo = true;
-        while (isTalkingTo)
-        {
-            for (int i = 0; i < Options.Count(); i++)
-            {
-                System.Console.WriteLine($"{i + 1}. {Options[i].Prompt}");
-            }
-
-            //Takes input converts index into Options array
-            int input = Int32.Parse(Console.ReadLine()) - 1;
-            var chosenOption = Options[input];
-            if (chosenOption.Reply != null)
-            {
-                System.Console.WriteLine(chosenOption.Reply);
-            }
-
-            if (chosenOption.Type == OptionType.CLOSE_DIALOGUE)
-            {
-                isTalkingTo = false;
-            }
-            if (chosenOption.Type == OptionType.ADD_INVENTORY_ITEM)
-            {
-                // Inventory.Add(item);
-            }
-
-        }
+        Dialogues[dialogueNumber].Start(NpcReply.GREETING);
     }
 }
