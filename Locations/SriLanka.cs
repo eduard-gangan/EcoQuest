664b16482deaae1113b628b1b3ea243e16426990
Your Name <xdeividaspetrulis@gmail.com>
2024-11-02 13:02:54 +0100

    Town hall was set to south instead of west

--- Locations/SriLanka.cs
+++ Locations/SriLanka.cs
@@ -13,7 +13,7 @@ namespace EcoQuest
             Room? srilanka_townhall = new("Town Hall", "You are at the Town Hall, you can see the mayor's office but the door is closed and you're not allowed in, he doesn't speak to no names.");
             Room? srilanka_recycling_station = new("Recycling Station", "You are at the Recycling Station, currently it is not working.");
 
-            srilanka_port.SetExits(srilanka_recycling_station, srilanka_beach, srilanka_townhall, null);
+            srilanka_port.SetExits(srilanka_recycling_station, srilanka_beach, null, srilanka_townhall);
             srilanka_beach.SetExit("west", srilanka_port);
             srilanka_recycling_station.SetExit("south", srilanka_port);
             srilanka_townhall.SetExit("east", srilanka_port);
