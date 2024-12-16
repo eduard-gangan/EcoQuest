e8efdd68ae0c6d3ebc33a3a800042cfb97ab38fc
victorptrc <108744480+victorptrc@users.noreply.github.com>
2024-12-16 23:45:27 +0200

    resolved issue

--- FishMinigame/Fish.cs
+++ FishMinigame/Fish.cs
@@ -4,7 +4,6 @@ namespace EcoQuest;
 public class Fish
 {
     public bool Endangered { get; private set; }
-    public bool Endangered { get; private set; }
     public string? Description { get; set; }
     public string? Name { get; set; }
     public bool Found { get; set; }
@@ -12,7 +11,7 @@ public class Fish
     public string ID { get; set; }
     public CanvasImage Image { get; set; }
 
-    public Fish(bool endangered, string description, string name, string imagePath, string id)
+
     public Fish(bool endangered, string description, string name, string imagePath, string id)
     {
         Endangered = endangered;
